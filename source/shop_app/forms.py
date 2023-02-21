080561d175dc6bfefec37761d87bb882c3100d39
Dias Daribayev <fission.frames-0s@icloud.com>
2023-02-21 10:48:27 +0600

    Added forms.py

---
+++ source/shop_app/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from .models import Shop, Category
+
+
+class ShopForm(forms.ModelForm):
+    class Meta:
+        model = Shop
+        fields = ['title', 'price', 'image_url', 'category', 'description']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control'}),
+            'image_url': forms.URLInput(attrs={'class': 'form-control'}),
+            'category': forms.Select(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+        }
+
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model = Category
+        fields = ['name']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+        }
