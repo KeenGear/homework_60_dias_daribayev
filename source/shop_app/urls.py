6c604aca0c37184afad4eb743da6767588ccac92
Dias Daribayev <fission.frames-0s@icloud.com>
2023-03-13 17:16:11 +0600

    added urls.py and views.py

--- source/shop_app/urls.py
+++ source/shop_app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import ProductList, ProductCreate, ProductDelete, ProductUpdate, ProductView, CategoryList, CategoryCreate, CategoryUpdate, CategoryDelete
+from .views import ProductList, ProductCreate, ProductDelete, ProductUpdate, \
+    ProductView, CategoryList, CategoryCreate, CategoryUpdate, \
+    CategoryDelete, cart_detail, CartRemoveView, cart_add, cart_update, checkout, checkout_success
 
 urlpatterns = [
     path('', ProductList.as_view(), name='products_list'),
@@ -11,4 +13,10 @@ urlpatterns = [
     path('categories/add/', CategoryCreate.as_view(), name='category_create'),
     path('categories/<int:pk>/edit', CategoryUpdate.as_view(), name='category_update'),
     path('categories/<int:pk>/delete', CategoryDelete.as_view(), name='category_delete'),
+    path('cart/', cart_detail, name='cart_detail'),
+    path('cart/remove/<int:shop_id>/', CartRemoveView.as_view(), name='cart_remove'),
+    path('cart/add/<int:shop_id>/', cart_add, name='cart_add'),
+    path('cart/update/<int:item_id>/', cart_update, name='cart_update'),
+    path('checkout/', checkout, name='checkout'),
+    path('checkout_success/', checkout_success, name='checkout_success')
 ]
--- source/shop_app/views.py
+++ source/shop_app/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from .models import Shop, Category
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import View
+
+from .models import Shop, Category, Cart, CartItem
 from django.views.generic import DetailView, ListView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.urls import reverse_lazy
-from .forms import ShopForm, CategoryForm
+from .forms import ShopForm, CategoryForm, CartItemForm, CheckoutForm
 
 
 class ProductView(DetailView):
@@ -75,9 +77,79 @@ class CategoryUpdate(UpdateView):
     model = Category
     form_class = CategoryForm
     template_name = 'category_form.html'
+    success_url = reverse_lazy('category_list')
 
 
 class CategoryDelete(DeleteView):
     model = Category
     success_url = reverse_lazy('category_list')
     template_name = 'category_confirm_delete.html'
+
+
+def cart_detail(request):
+    cart = Cart.objects.first()
+    checkout_form = CheckoutForm()
+    if request.method == 'POST':
+        checkout_form = CheckoutForm(request.POST)
+        if checkout_form.is_valid():
+            checkout_form.save()
+
+    context = {
+        'cart': cart,
+        'checkout_form': checkout_form,
+    }
+    return render(request, 'cart_detail.html', context)
+
+
+def cart_add(request, shop_id):
+    cart = Cart.objects.first()
+    if not cart:
+        cart = Cart.objects.create()
+    shop = get_object_or_404(Shop, id=shop_id)
+    cart.add(shop)
+    return redirect('cart_detail')
+
+
+class CartRemoveView(View):
+    def get(self, request, shop_id):
+        cart = Cart.objects.first()
+        cart.remove(shop_id)
+        return redirect('cart_detail')
+
+    def post(self, request, shop_id):
+        cart = Cart.objects.first()
+        cart.remove(shop_id)
+        return redirect('cart_detail')
+
+
+def cart_update(request, item_id):
+    cart_item = get_object_or_404(CartItem, id=item_id)
+    if request.method == 'POST':
+        form = CartItemForm(request.POST, instance=cart_item)
+        if form.is_valid():
+            form.save()
+            return redirect('cart_detail')
+    else:
+        form = CartItemForm(instance=cart_item)
+
+    context = {
+        'form': form,
+        'cart_item': cart_item,
+    }
+    return render(request, 'cart_update.html', context)
+
+
+def checkout(request):
+    if request.method == 'POST':
+        form = CheckoutForm(request.POST)
+        if form.is_valid():
+            checkout = form.save()
+            return redirect('checkout_success')
+    else:
+        form = CheckoutForm()
+    context = {'form': form}
+    return render(request, 'checkout.html', context)
+
+
+def checkout_success(request):
+    return render(request, 'checkout_success.html')
