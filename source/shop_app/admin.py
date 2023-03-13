953b9214f8afc275303e2f5342f5d6dd32c0d851
Dias Daribayev <fission.frames-0s@icloud.com>
2023-03-13 17:15:50 +0600

    Registered in admin.py also forgot add product_list.html

--- source/shop_app/admin.py
+++ source/shop_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Category, Shop, Cart, CartItem
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Shop)
+admin.site.register(Cart)
+admin.site.register(CartItem)
